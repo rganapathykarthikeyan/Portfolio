c3c3fe2ba614ee563278ab5542c05405ccc40ced
Ganapathy Karthikeyan <rganapathykarthikeyan8@gmail.com>
2023-12-19 03:47:36 +0530

    styling

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -7,9 +7,9 @@ import {motion} from 'framer-motion';
 const Links = () => {
     return (
         <div className='flex flex-row p-4 w-full justify-between items-center border-b lg:border-b-0 lg:w-fit lg:flex-col'>
-            <motion.div className='flex h-full justify-center items-center text-[#ffffff] lg:hidden font-Raleway text-xl sm:text-2xl'
+            <motion.div className='flex h-full justify-center items-center text-[#ffffff] lg:hidden font-Teko text-2xl sm:text-4xl'
                 initial={{opacity: 0, scale: 0.5}} whileInView={{opacity:1, scale:1}} transition={{duration: 0.8}}>
-                React Developer
+                REACT DEVELOPER
             </motion.div>
             <motion.div className='flex flex-row lg:p-6 lg:flex-col'
                 initial={{opacity: 0, x:-25}} whileInView={{opacity:1, x:0}} transition={{duration: 1.5}}>
